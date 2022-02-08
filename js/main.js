87eba4891665495c1196745640808397b0607735
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-02-08 23:43:38 +0500

    finished till footer

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let elMenuBtn = document.getElementById("menuBtn");
+let elNavBox = document.getElementById("navBox");
+let elIconBtn = document.getElementById("iconBtn");
+let count = 0;
+
+elMenuBtn.addEventListener('click', function() {
+  elNavBox.classList.toggle('show');
+  elMenuBtn.classList.toggle('menu-show');
+  if (count == 0){
+    elIconBtn.className = "bx bx-x";
+    count++;
+  } else{
+    elIconBtn.className = "bx bx-menu";
+    count--;
+  }
+})
